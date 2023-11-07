c2c6b79f4a743f186f749c3c12158cd812f409d3
Simon <simonlucas1996@gmail.com>
2023-11-07 01:19:21 -0300

    Set same_site None in singin cookie

--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -10,6 +10,7 @@ class SigninController < ApplicationController
       response.set_cookie(JWTSessions.access_cookie,
                         value: tokens[:access],
                         httponly: true,
+                        same_site: "None",
                         secure: Rails.env.production?)
       render json: { csrf: tokens[:csrf], user_id: user.id }
     else
